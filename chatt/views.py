3b0cb5168f936e2dc1f0ab3daea9f2b6096de432
Ayanda Dladla <mseshidladla@gmail.com>
2022-05-16 16:14:11 +0200

    first

---
+++ chatt/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+from chatt.models import Thread
+
+
+@login_required
+def messages_page(request):
+    threads = Thread.objects.by_user(user=request.user).prefetch_related('chatmessage_thread').order_by('timestamp')
+    context = {
+        'Threads': threads
+    }
+    return render(request, 'messages.html', context)
